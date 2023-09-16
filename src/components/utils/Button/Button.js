920ba550a784b3067ceb3f10bb7467310d518751
dwightwu814 <dwightwu@hotmail.com>
2023-09-16 16:14:44 -0400

    Fixed button problem

--- src/components/utils/Button/Button.js
+++ src/components/utils/Button/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Button.scss";
 
-export default function Button({ icon, text, onClick }) {
+export default function Button({ icon, text, handleClick }) {
   return (
-    <button className="button">
+    <button type="button" className="button" onClick={handleClick}>
       <img className="button__icon" src={icon} alt="icon" />
       {text}
     </button>
