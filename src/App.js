7bbc7f57b7a700822b2a43cbaaf38afe599b69b3
dwightwu814 <dwightwu@hotmail.com>
2023-09-16 11:50:04 -0400

    Router architecture for main component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,17 @@
 import "./App.scss";
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
-import Header from "./components/Header/Header";
-import Comments from "./components/Comments/Comments";
-import NextVideosGallery from "./components/NextVideosGallery/NextVideosGallery";
-import axios from "axios";
-import apiKey from "./data/api-key.js";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PlayerPage from "./pages/Player/Player";
 
 function App() {
-  const [videos, setVideos] = useState([]);
-  const [currentVideoId, setCurrentVideoId] = useState(null);
-  const [currentVideo, setCurrentVideo] = useState(null);
-
-  const urlRoot = "https://project-2-api.herokuapp.com/";
-
-  // Get list of videos on load
-  useEffect(() => {
-    axios
-      .get(`${urlRoot}videos?api_key=${apiKey}}`)
-      .then((res) => {
-        setVideos(res.data);
-        setCurrentVideoId(res.data[0].id);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  // Get current video on currentVideoId change
-  useEffect(() => {
-    if (!currentVideoId) return;
-    axios
-      .get(`${urlRoot}videos/${currentVideoId}?api_key=${apiKey}}`)
-      .then((res) => {
-        setCurrentVideo(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [currentVideoId]);
-
-  const nextVideos = videos.filter((video) => {
-    return video.id !== currentVideoId;
-  });
-
   return (
-    <>
-      <Navbar />
-      <VideoPlayer posterUrl={currentVideo ? currentVideo.image : null} />
-      <section className="section below">
-        <section className="below__first">
-          <Header video={currentVideo} />
-          <Comments comments={currentVideo ? currentVideo.comments : []} />
-        </section>
-        <section className="below__second">
-          <NextVideosGallery
-            videos={nextVideos}
-            handleClick={(id) => {
-              setCurrentVideoId(id);
-            }}
-          />
-        </section>
-      </section>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PlayerPage />} />
+        <Route path="home" element={<PlayerPage />} />
+        <Route path="video/:videoId" element={<PlayerPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
