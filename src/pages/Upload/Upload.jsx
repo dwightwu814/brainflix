a7ee9b6e29d94bd3f61e2a088dcb7dcc29cb67a8
dwightwu814 <dwightwu@hotmail.com>
2023-09-16 16:15:07 -0400

    handle upload button click

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Upload.scss";
 import UploadThumbnail from "../../assets/images/upload-thumbnail.jpg";
